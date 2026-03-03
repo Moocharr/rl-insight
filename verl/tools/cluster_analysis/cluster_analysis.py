e95ca746128587849179144376d6d119c4ca7f10
X. HU <huxiaobo@zju.edu.cn>
2026-03-03 12:52:20 +0800

    [mstx] feat: scheduling analysis based on profiling data.
    Co-authored-by: wayne <mayunaise@qq.com>

--- verl/tools/cluster_analysis/cluster_analysis.py
+++ verl/tools/cluster_analysis/cluster_analysis.py
@@ -1,23 +1,46 @@
+# Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
-from parser import get_cluster_parser_fn
+
+from parser import get_cluster_parser_cls
+from schema import Constant
 from visualizer import get_cluster_visualizer_fn
-from omegaconf import DictConfig
+
 
 def main():
-    arg_parser = argparse.ArgumentParser(description="集群调度可视化")
-    arg_parser.add_argument("--input-path", default="test", help="profiling数据的原始路径")
-    arg_parser.add_argument("--profiler-type", default="mstx", help="性能数据种类")
-    arg_parser.add_argument("--output-path", default="test", help="输出路径")
-    arg_parser.add_argument("--vis-type", default="html", help="可视化类型")
+    arg_parser = argparse.ArgumentParser(description="Cluster scheduling visualization")
+    arg_parser.add_argument("--input-path", default="test", help="Raw path of profiling data")
+    arg_parser.add_argument("--profiler-type", default="mstx", help="Profiler type, supported mstx/nvtx")
+    arg_parser.add_argument("--output-path", default="test", help="Output path")
+    arg_parser.add_argument("--vis-type", default="html", help="Visualization type, supported html")
+    arg_parser.add_argument("--rank-list", type=str, help="Rank id list", default="all")
     args = arg_parser.parse_args()
 
-    parser_config = DictConfig({})
-    visualizer_config = DictConfig({})
+    # Prepare parser configuration
+    parser_config = {
+        Constant.INPUT_PATH: args.input_path,
+        Constant.RANK_LIST: args.rank_list,
+    }
+    visualizer_config = {}
 
-    # 选择解析方式
-    parser_fn = get_cluster_parser_fn(args.profiler_type)
-    data = parser_fn(args.input_path, args.output_path, parser_config)
-    # 选择输出方式
+    # Get and call parser
+    parser_cls = get_cluster_parser_cls(args.profiler_type)
+    parser = parser_cls(parser_config)
+    data = parser.parse()
+
+    # Get and Call visualizer
     visualizer_fn = get_cluster_visualizer_fn(args.vis_type)
     visualizer_fn(data, args.output_path, visualizer_config)
 
